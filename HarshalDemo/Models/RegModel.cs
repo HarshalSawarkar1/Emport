a3b3117bf6c3ceb915075318b55f671c4396a9d4
SagarAnbhore <sagaranbhore1998@gmail.com>
2022-12-02 14:35:48 +0530

    Add project files.

---
+++ HarshalDemo/Models/RegModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HarshalDemo.Models
+{
+    public class RegModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+    }
+}
